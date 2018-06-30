48065d99493dcde853793b1949595df733f04f7c
Jeury Mejia <jeury.mejia@gmail.com>
2018-06-30 13:45:59 -0400

    Add morsel-solutions to matrix_from_string

--- 6. matrix_from_string/matrix.py
+++ 6. matrix_from_string/matrix.py
@@ -5,18 +5,22 @@ def matrix_from_string(mtx_str):
     into an actual matrix. This function uses \n as a delimiter for the rows,
     and ignores any extra spaces.
 
-    Args: 
+    Turned into a one-liner comprehension, it's equivalent was commented out.
+
+    Args:
         mtx_str: A string representation of a matrix
 
     Returns:
         An actual matrix for the string
     """
-    mtx_rows = mtx_str.split("\n")
-    final_matrix = []
 
-    for row in mtx_rows:
-        if row.replace(" ", "") != "":
-            mtx_row = [float(col) for col in row.strip().split(" ") if col != ""]
-            final_matrix.append(mtx_row)
-    return final_matrix
-
+    # mtx_rows = mtx_str.split("\n")
+    # final_matrix = []
+    #
+    # for row in mtx_rows:
+    #     if row.replace(" ", "") != "":
+    #         mtx_row = [float(col) for col in row.strip().split(" ") if col != ""]
+    #         final_matrix.append(mtx_row)
+    # return final_matrix
+    return ([[float(col) for col in row.strip().split(" ") if col != ""]
+        for row in mtx_str.split("\n") if row.replace(" ", "") != ""])
